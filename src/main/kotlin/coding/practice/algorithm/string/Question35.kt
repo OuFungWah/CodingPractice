20ae3049407feda566176edec8a7bd6c5d6bc516
oufenghua <oufenghua@bytedance.com>
2020-07-28 22:14:25 +0800

    20200728 晚上刷题

--- src/main/kotlin/coding/practice/algorithm/string/Question35.kt
+++ src/main/kotlin/coding/practice/algorithm/string/Question35.kt
@@ -1,6 +1,6 @@
 package coding.practice.algorithm.string
 
-/*
+/**
 给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
 
 示例 1:
@@ -16,4 +16,33 @@ package coding.practice.algorithm.string
 
 进阶:
 如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
-* */
+
+@see <a href="https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/35/">链接</a>
+ * */
+
+fun main(){
+    println(Question35().isAnagram("anagram", "nagaram"))
+}
+
+class Question35{
+
+    /**
+     * 思路：
+     * 1. 长度
+     * 2. 元素种类
+     * 3. 元素各自数量，s 加，t 减
+     * 4. 最后看各个元素的剩余数量
+     */
+    fun isAnagram(s: String, t: String): Boolean {
+        if(s.length != t.length) return false
+        val arr = Array(26) { 0 }
+        val zeroPoint = 'a'.toInt()
+        s.forEach { word: Char ->
+            arr[word.toInt() - zeroPoint] += 1
+        }
+        t.forEach {  word: Char ->
+            arr[word.toInt() - zeroPoint] -= 1
+        }
+        return !arr.any { it != 0 }
+    }
+}
--- src/main/kotlin/coding/practice/algorithm/string/Question36.kt
+++ src/main/kotlin/coding/practice/algorithm/string/Question36.kt
@@ -1,48 +1,134 @@
 package coding.practice.algorithm.string
 
-/**
-给定两个字符串 s 和 t ，编写一个函数来判断 t 是否是 s 的字母异位词。
+import java.util.*
 
-示例 1:
-
-输入: s = "anagram", t = "nagaram"
-输出: true
-示例 2:
-
-输入: s = "rat", t = "car"
-输出: false
-说明:
-你可以假设字符串只包含小写字母。
-
-进阶:
-如果输入字符串包含 unicode 字符怎么办？你能否调整你的解法来应对这种情况？
-
- @see <a href="https://leetcode-cn.com/explore/interview/card/top-interview-questions-easy/5/strings/35/">链接</a>
- * */
-
-fun main(){
-    println(Question36().isAnagram("anagram", "nagaram"))
+fun main() {
+    println(Question36().isPalindrome("A man, a plan, a canal: Panama"))
+    println(Question36().isPalindrome("."))
+    println(Question36().isPalindrome("a."))
+    println(Question36().isPalindrome("aa"))
+    println(Question36().isPalindrome("0P"))
+    println(Question36().isPalindrome(".,"))
 }
 
-class Question36{
+class Question36 {
+
+    /**
+     * 思路：去除无用字符，然后用栈操作
+     */
+    fun isPalindrome2(s: String): Boolean {
+        val range = '0'.toInt()..'z'.toInt()
+        val realContent = s.filter {
+            it.toInt() in range && it != ':'
+        }.toLowerCase()
+        println(realContent)
+        val stack = Stack<Char>()
+        val midPoint = realContent.length / 2
+        val isEven = realContent.length % 2 == 0
+        for (index in 0 until realContent.length) {
+            val curChar = realContent[index]
+            when (index) {
+                in (0 until midPoint) -> {
+                    stack.push(curChar)
+                }
+                in (midPoint until realContent.length) -> {
+                    if (index != midPoint || isEven) {
+                        if (stack.lastElement() == curChar) {
+                            stack.pop()
+                        }
+                    }
+                }
+            }
+        }
+        return stack.size == 0
+    }
+
+    private val charRangeH = 'A'.toInt()..'Z'.toInt()
+    private val charRangeL = 'a'.toInt()..'z'.toInt()
+    private val numRange = '0'.toInt()..'9'.toInt()
+
+    private fun Char.isVail(): Boolean = this.toInt() in charRangeH || this.toInt() in charRangeL || this.toInt() in numRange
 
     /**
      * 思路：
-     * 1. 长度
-     * 2. 元素种类
-     * 3. 元素各自数量，s 加，t 减
-     * 4. 最后看各个元素的剩余数量
+     * 1. 收尾同时开始遍历，只看有效字符
+     * 2. 若有效字符相等，继续遍历。不相等则不是回文串
+     * 3. 直到头尾两个游标相交（i > j）
+     *
+     * 注意：
+     * 1. 游标相等也要判断，因为一个字符也是回文
+     * 2. 注意一个有效字符都没有发现时判断为空串，空串在题目中也算回文
+     *
+     * 耗时 208ms
      */
-    fun isAnagram(s: String, t: String): Boolean {
-        if(s.length != t.length) return false
-        val arr = Array(26) { 0 }
-        val zeroPoint = 'a'.toInt()
-        s.forEach { word: Char ->
-            arr[word.toInt() - zeroPoint] += 1
+    fun isPalindrome(s: String): Boolean {
+        if (s.length <= 1) return true
+        println(s)
+        var i = 0
+        var j = s.length - 1
+        var result = false
+        while (i < j) {
+            while (!s[i].toLowerCase().isVail() && i + 1 < s.length) i++
+            while (!s[j].toLowerCase().isVail() && j - 1 >= 0) j--
+            if (i == s.length - 1 && j == 0) return true
+            println("content[$i] ${s[i]} == content[$j] ${s[j]} : ${(s[i] == s[j])} ")
+            if (i > j) {
+                break
+            }
+            result = s[i].toLowerCase() == s[j].toLowerCase()
+            if (!result) break
+            i++
+            j--
         }
-        t.forEach {  word: Char ->
-            arr[word.toInt() - zeroPoint] -= 1
-        }
-        return !arr.any { it != 0 }
+        return result
     }
+
+    /**
+     * 耗时 240 ms
+     */
+    fun isPalindrome3(s: String): Boolean {
+        val content = s.toLowerCase().filter { it.isVail() }
+        if (content.length <= 1) return true
+        println(content)
+        var i = 0
+        var j = content.length - 1
+        var result = false
+        while (i < j) {
+
+            if (i == content.length - 1 && j == 0) return true
+            if (i > j) {
+                break
+            }
+            result = content[i] == content[j]
+            if (!result) break
+            i++
+            j--
+        }
+        return result
+    }
+
+    /**
+     * 耗时 264 ms
+     */
+    fun isPalindrome4(s: String): Boolean {
+        val content = s.filter { it.isVail() }.toLowerCase()
+        if (content.length <= 1) return true
+        println(content)
+        var i = 0
+        var j = content.length - 1
+        var result = false
+        while (i < j) {
+
+            if (i == content.length - 1 && j == 0) return true
+            if (i > j) {
+                break
+            }
+            result = content[i] == content[j]
+            if (!result) break
+            i++
+            j--
+        }
+        return result
+    }
+
 }
