d31fa4c9a828a77913a294d5a267bbe2f44c46d9
oufenghua <oufenghua@bytedance.com>
2020-07-31 11:17:03 +0800

    20200731 早上刷题

--- src/main/kotlin/coding/practice/algorithm/string/Question38.kt
+++ src/main/kotlin/coding/practice/algorithm/string/Question38.kt
@@ -18,6 +18,12 @@ class Question38 {
     /**
      * 耗时：16% ～ 0%
      *
+     * 思想：
+     * 1、逐个遍历主串
+     * 2、逐个遍历子串
+     * 3、如果其中一个字符不匹配，主串游标位置不变，挪动子串游标到 table[子串游标] 处，以复用前面的匹配结果。
+     * 4、如果 table[子串游标] 为 -1，主串和子串的游标一起继续往后挪。
+     *
      * TODO ofh 很奇怪，为什么明明是 KMP 算法，但是效率却比很多其他算法慢呢
      */
     fun strStr(haystack: String, needle: String): Int {
@@ -54,11 +60,7 @@ class Question38 {
      *
      */
     fun tableBuilding(pattern: String): Array<Int> {
-        val table = Array(pattern.length) { if(it == 0) -1 else 0 }
-//        pattern.forEachIndexed { index, c ->
-//            if (index == 0 || index == pattern.length - 1) return@forEachIndexed
-//            if (pattern[table[index]] == c) table[index + 1] = table[index] + 1
-//        }
+        val table = Array(pattern.length) { if (it == 0) -1 else 0 }
         for (index in 2 until pattern.length) {
             if (pattern[index - 1] == pattern[table[index - 1]]) {
                 table[index] = table[index - 1] + 1
@@ -70,18 +72,3 @@ class Question38 {
         return table
     }
 }
-
-/*
-* aabaaac
-* 0010122
-*
-* abcd
-* aabaaac
-*
-* aabaa
-* 00101
-*
-* abcd
-* abc
-*
-* */
