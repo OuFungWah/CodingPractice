ffe81a5026e943f97f9d6231b5534e8272556d49
oufenghua <oufenghua@bytedance.com>
2020-08-04 23:29:18 +0800

    commit some question

---
+++ src/main/kotlin/coding/practice/algorithm/link/Question41.kt
@@ -0,0 +1,38 @@
+package coding.practice.algorithm.link
+
+/**
+ * Example:
+ * var li = ListNode(5)
+ * var v = li.`val`
+ * Definition for singly-linked list.
+ * class ListNode(var `val`: Int) {
+ *     var next: ListNode? = null
+ * }
+ */
+
+class ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+class Solution {
+    val head = ListNode(5).apply {
+        next = ListNode(4).apply {
+            next = ListNode(1).apply {
+                next = ListNode(9)
+            }
+        }
+    }
+    var li = ListNode(5)
+    var v = li.`val`
+    fun deleteNode(node: ListNode?) {
+        var current: ListNode? = ListNode(0)
+        current?.next = head
+        do {
+            if (current?.next?.`val` != null && current.next?.`val` == node?.`val`) {
+                current?.next = current.next?.next
+                break
+            }
+            current = current?.next
+        } while (current?.next != null)
+    }
+}
