3956a543501d25a717a8ccffd75c2c76ed41367b
oufenghua <oufenghua@bytedance.com>
2020-10-03 14:35:59 +0800

    2

--- src/main/kotlin/coding/practice/test/GitRebaseTest.kt
+++ src/main/kotlin/coding/practice/test/GitRebaseTest.kt
@@ -1,5 +1,35 @@
 package coding.practice.test
 
+fun main(){
+//    println(Integer.toBinaryString(8))
+    val testing = Testing()
+    Thread{
+        testing.method("A")
+    }.start()
+
+    Thread{
+        testing.method("B")
+    }.start()
+}
+
+class Testing{
+
+    val obj = Any()
+
+    fun method(name : String){
+        println("$name sleep start")
+        synchronized(obj){
+            var time = 0
+            while (time < 3){
+                println("$name using")
+                Thread.sleep(1000)
+                time++
+            }
+        }
+        println("$name sleep end")
+    }
+}
+
 class GitRebaseTest {
 
     // 在 Home  分支做修改
